cc7cb438cdc846c72d5f73ab4bd73902ff74ea42
mouadia <ouadia.mohammed@outlook.de>
2023-06-22 22:28:30 +0200

    sub

--- OP_functions.c
+++ OP_functions.c
@@ -23,3 +23,28 @@ void    _add(stack_t **stack, unsigned int line_number)
 	(*stack)->n = number;
 
 }
+
+/**
+ * _sub - subtracts the top element of the stack
+ *        from the second top element of the stack.
+ * @stack: stack.
+ * @line_number: line number.
+ * Return: void.
+ */
+
+void	_sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *number;
+	int res;
+
+	number = *stack;
+	if (!number || !(number->next))
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	res = number->next->n - number->n;
+	number->next->n = res;
+	_pop(stack, line_number);
+}
--- monty.h
+++ monty.h
@@ -50,6 +50,7 @@ void	_pop(stack_t **stack, unsigned int line_number);
 void    _swap(stack_t **stack, unsigned int line_number);
 void    _add(stack_t **stack, unsigned int line_number);
 void	_nop(__attribute__ ((unused))stack_t **stack, __attribute__ ((unused))unsigned int line_number);
+void	_sub(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *head);
 
 #endif
